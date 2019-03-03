8bcd66108280bea3ed862bb37610fa36ee3c4609
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-03-03 15:48:27 -0500

    Added documentation

--- Stepper/src/Stepper/Stepper.cpp
+++ Stepper/src/Stepper/Stepper.cpp
@@ -1,33 +1,59 @@
+/**
+	Stepper.hpp
+	Purpose: Library to be used with the TB6600 Stepper Driver and a Raspberry Pi
+	
+	@author Kaushik Prakash
+	@version 1.0 03/01/19
+*/
+
+
 #include "Stepper.hpp"
-#include <iostream>
 #include <wiringPi.h>
 
-Stepper::Stepper(const int directionPin, const int pulsePin, const int microStepSize) {
-	wiringPiSetupGpio();
-	_directionPin = directionPin;
-	_pulsePin = pulsePin;
+Stepper::Stepper(const int DIRECTION_PIN, const int PULSE_PIN, const int MICRO_STEP_SIZE) {
+	wiringPiSetupGpio(); // Set up wiringPi with original pin mapping
+	
+	_directionPin = DIRECTION_PIN;
+	_pulsePin = PULSE_PIN;
+	_microStepSize = MICRO_STEP_SIZE;
+	_currPosition = 0;
+	// Set the direction and pulse pins as output
 	pinMode(_directionPin, OUTPUT);
 	pinMode(_pulsePin, OUTPUT);
-	_microStepSize = microStepSize;
 }
 
-void Stepper::absStep(const int desiredPosition) {
-	const int numSteps = desiredPosition - _currPosition;
-	relStep(numSteps);
-	_currPosition = desiredPosition;
+/**
+	Tells the stepper motor to go to DESIRED_POSITION from _currPosition, which is 0 on startup
+	
+	@param DESIRED_POSITION Desired final position of the shaft
+*/
+
+void Stepper::absStep(const int DESIRED_POSITION) {
+	const int NUM_STEPS = DESIRED_POSITION - _currPosition; // Number of steps to take to reach DESIRED_POSITION
+	relStep(NUM_STEPS); // Move to reach DESIRED_POSITION
+	_currPosition = DESIRED_POSITION; // DESIRED_POSITION has been reached
 }
 
-
-void Stepper::relStep(const int steps) {
-	if(steps > 0) {
-		moveForward(steps);
+/**
+	Tells the stepper motor to move a certain number of steps
+	
+	@param STEPS Number of steps to take
+*/
+void Stepper::relStep(const int STEPS) {
+	if(STEPS > 0) {
+		moveForward(STEPS);
 	} else {
-		moveBackward(steps * -1);
+		moveBackward(STEPS * -1); // Multiply by -1 because the direction change is handled by _directionPin
 	}
 }
 
-void Stepper::moveForward(const int steps) {
-	for(int i = 0; i < steps; i += 1) {
+/**
+	Moves the stepper motor forward
+	
+	@param STEPS Number of steps to take
+*/
+void Stepper::moveForward(const int STEPS) {
+	for(int i = 0; i < STEPS; i += 1) {
 		digitalWrite(_directionPin, LOW);
 		digitalWrite(_pulsePin, HIGH);
 		delayMicroseconds(50);
@@ -36,8 +62,13 @@ void Stepper::moveForward(const int steps) {
 	}
 }
 
-void Stepper::moveBackward(const int steps) {
-	for(int i = 0; i < steps; i += 1) {
+/**
+	Moves the stepper motor backward
+	
+	@param STEPS Number of steps to take
+*/
+void Stepper::moveBackward(const int STEPS) {
+	for(int i = 0; i < STEPS; i += 1) {
 		digitalWrite(_directionPin, HIGH);
 		digitalWrite(_pulsePin, HIGH);
 		delayMicroseconds(50);
--- Stepper/src/Stepper/Stepper.hpp
+++ Stepper/src/Stepper/Stepper.hpp
@@ -1,13 +1,22 @@
+/**
+	Stepper.hpp
+	Purpose: Library to be used with the TB6600 Stepper Driver and a Raspberry Pi
+
+	@author Kaushik Prakash
+	@version 1.0 03/01/19
+*/
+
 class Stepper {
-	public:
-		Stepper(const int directionPin, const int pulsePin, const int microStepSize);
-		void relStep(const int steps);
-		void absStep(const int desiredPosition);
-	private:
-		int _microStepSize;
-		int _directionPin;
-		int _pulsePin;
-		int _currPosition;
-		void moveForward(const int steps);
-		void moveBackward(const int steps);
+
+public:
+    Stepper(const int DIRECTION_PIN, const int PULSE_PIN, const int MICRO_STEP_SIZE);	// Constructor
+    void relStep(const int STEPS); // Relative motor stepping; No software tracking of shaft position
+    void absStep(const int DESIRED_POSITION); // Absolute motor steppering with software tracking
+private:
+    int _microStepSize;
+    int _directionPin;
+    int _pulsePin;
+    int _currPosition;
+    void moveForward(const int STEPS);
+    void moveBackward(const int STEPS);
 };
